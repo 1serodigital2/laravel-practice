1afb5a54ea65f592a84a6f1715b1c7ea3b2fe6e7
unknown <digitalbijay17@gmail.com>
2024-05-18 07:44:57 +0530

    created category seeder

---
+++ database/seeders/NightstaySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\nightstay;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class NightstaySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // nightstay::create([
+        //     'name' => 'Dooars',
+
+        // ])
+    }
+}
