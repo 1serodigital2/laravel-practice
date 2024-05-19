8d376434e3b9d1bad43a9961191ee3e18beccc38
unknown <digitalbijay17@gmail.com>
2024-05-19 16:50:43 +0530

    started

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <nav>
+        <a href="{{ route('home') }}">Home</a>
+        <a href="{{ route('home') }}">Home</a>
+        <a href="{{ route('home') }}">Home</a>
+    </nav>
+</header>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+@section('content')
+    <h1>homepage</h1>
+@endsection
